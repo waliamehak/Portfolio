423830f7ad816f6a535ec6c241be7b8ff0e530ea
Mehak Walia <waliamehakjeet@gmail.com>
2021-10-04 21:13:41 -0400

    Updated the projects page

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,51 +3,38 @@ import {v4 as uuidv4} from "uuid"
 export const projects = [
     {
         id:uuidv4(), 
-        title: 'Project One', 
-        desc: 'Helllo', 
-        github:  '',
-        demo:'',
+        title: 'LearnML', 
+        desc: 'An educative platform that introduces its users to Classification, Clustering and Regression.', 
+        github:  'https://github.com/waliamehak/LearnML',
         image:'./images/project1.jpg'
     },
     {
         id:uuidv4(), 
-        title: 'Project One', 
-        desc: 'Helllo', 
-        github:  '',
-        demo:'',
+        title: 'Startle', 
+        desc: 'An Android application that pranks other users. A pop-up of a scary image appears along with a sound whenever a user clicks on the screen.', 
+        github:  'https://github.com/waliamehak/finalAPP_CS855',
         image:'./images/project2.jpg'
     },
     {
         id:uuidv4(), 
-        title: 'Project Two', 
-        desc: 'Helllo', 
-        github:  '',
-        demo:'',
-        image:'./images/project3.jpg'
+        title: 'Sorting Visaliser', 
+        desc: 'A website that visualizes the mechanics behind Merge sort, Bubble sort, Quick sort & Insertion sort.', 
+        github:  'https://github.com/waliamehak/SortingVisualiser',
+        image:'./images/project3.png'
     },
     {
         id:uuidv4(), 
-        title: 'Project Three', 
-        desc: 'Helllo', 
-        github:  '',
-        demo:'',
-        image:'./images/project4.jpg'
+        title: 'Candy Analysis', 
+        desc: 'A Jupyter notebook that creates a logistic regression model to look at the ingredients of a candy and then determines its likability.', 
+        github:  'https://github.com/waliamehak/predictiveAnalysisOfCandies',
+        image:'./images/project5.jpg'
     },
     {
         id:uuidv4(), 
-        title: 'Project Four', 
-        desc: 'Helllo', 
-        github:  '',
-        demo:'',
-        image:'./images/project4.jpg'
-    },
-    {
-        id:uuidv4(), 
-        title: 'Project Five', 
-        desc: 'Helllo', 
-        github:  '',
-        demo:'',
-        image:'./images/project4.jpg'
+        title: 'Ray Tracer', 
+        desc: 'C++ based code that renders spheres by ray-tracing and uses OpenMP for parallel execution.', 
+        github:  'https://github.com/waliamehak/Ray-Tracer',
+        image:'./images/project6.png'
     }
     
 ]
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,6 @@
 import React, {Fragment} from 'react';
 import {useState} from 'react';
 import { projects } from '../data/projects';
-import {FaGithub} from 'react-icons/fa'
-import {CgWebsite} from 'react-icons/cg'
 
 const Projects = () => {
     //eslint-disable-next-line
@@ -15,20 +13,11 @@ const Projects = () => {
                 <div className='cards'>
                 {cards.map(({id, title, desc, github, demo, image})=>
                 (
-                    <article key = {id}>
-                        <img src = {image} alt={title} height = '400px' wight  = '200px'/>
+                    <article className =  'article' key = {id}>
+                        <a href = {github} target="_blank" rel="noopenner noreferrer" >
+                        <img src = {image} alt={title} height = '450px' width  = '100px'/></a>
                         <h4>{title}</h4>
-                        <p>desc</p>
-                        <ul>
-                            <li><a 
-                            href= {github} 
-                            target="_blank" rel="noopenner noreferrer" 
-                            className="github"><FaGithub style= {{marginRight: 5}}/>Github</a></li>
-                            <li><a 
-                            href= {demo} 
-                            target="_blank" rel="noopenner noreferrer" 
-                            className="demo"><CgWebsite style = {{ marginRight: 5}}/>Demo</a></li>
-                        </ul>
+                        <p>{desc}</p>
                     </article>
                 )
                 )}
